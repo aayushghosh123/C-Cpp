7f9898962b781aa5dffb96755620d6370e9b9a07
geekghosh <ghoshaayush208@gmail.com>
2020-06-18 13:12:25 +0530

    Update Binary_Tree_to_Binary_Search_Tree_Conversion.cpp

--- DSA(ALL CODES IN C++)/BST/Binary_Tree_to_Binary_Search_Tree_Conversion.cpp
+++ DSA(ALL CODES IN C++)/BST/Binary_Tree_to_Binary_Search_Tree_Conversion.cpp
@@ -3,142 +3,141 @@ using namespace std;
 
 struct node{
     int data;
-    struct node *left;
-    struct node *right;
+    struct node *next;
 
     node(){
         data=0;
-        left = right = NULL;
+        next=NULL;
     }
 };
 
-struct node *root=NULL;
+struct node1{
+    int data=0;
+    struct node1 *left;
+    struct node1 *right;
+
+    node1(){
+        data=0;
+        left=right=NULL;
+    }
+};
+
 struct node *start=NULL;
-struct node *temp;
-int flag=0;
-int k=0,n=0;
+struct node1 *start1=NULL;
+struct node1 *temp1;
 
+void create_ll(int n){
+    int data=0;
+    start = new node();
+    cin>>data;
+    start->data = data;
 
-node *binary_tree(node *start,int data){
-
-    if(start==NULL){
-        start = new node();
-        start->data = data;
-        start->left = start->right = NULL;
-    }
-    else{
-    temp = start;
-    struct node *parent;
-    while(temp != NULL){
-        if(flag==0){
-            flag=1;
-            parent = temp;
-            temp = temp->left;
+    if(start!=NULL){
+        struct node *temp = start;
+        for(int i=1;i<n;i++){
+            struct node *newdata= new node();
+            cin>>data;
+            newdata->data = data;
+            newdata->next = NULL;
+            temp->next = newdata;
+            temp = temp->next;
         }
-        else if(flag==1){
-            flag=0;
-            parent = temp;
-            temp = temp->right;
-        }
-    }
-
-    if(flag==1){
-        parent->left = new node();
-        parent->left->data = data;
-    }
-    else{
-        parent->right = new node();
-        parent->right->data = data;
-    }
-    }
-    return start;
+    }   
 }
 
-int arr[ ]={0};
-
-
-void showdata(node *start){
-    if(start == NULL)
-    return;
-
-    showdata(start->left);
-    if(start->data){
-        arr[k]=start->data;
-        k++;
-    }
-    showdata(start->right);
-}
-
-void bst_tree(int data){
-    if(root == NULL){
-        root = new node();
-        root->data = data;
-        root->left = root->right = NULL;
+void bst(int data){
+    if(start1==NULL){
+        cout<<"start1 is NULL"<<endl;
+        start1 = new node1();
+        start1->data = data;
     }
     else{
-        temp = root;
-        struct node *parent;
+        temp1 = start1;
+        struct node1 *parent = start1;
+        while(temp1!=NULL){
+            if(temp1->data<data){
+                parent = temp1;
+                temp1 = temp1->right;
+            }
+            else{
+                parent = temp1;
+                temp1 = temp1->left;
+            }
+        }
 
-        while(temp != NULL){
-        if(temp->data > data){
-            parent = temp;
-            temp = temp->left;
-        }
-        else{
-            parent = temp;
-            temp = temp->right;
-        }
-        }
-
-        if(parent->data > data){
-            parent->left = new node();
-            parent->left->data = data;
-        }
-        else{
-            parent->right = new node();
+        if(parent->data<data){
+            parent->right = new node1();
             parent->right->data = data;
         }
+        else{
+            parent->left = new node1();
+            parent->left->data = data;
+        }
     }
 }
 
-void bst(int arr[]){
+void bst_tree_element(int n,int arr[]){
     for(int i=0;i<n;i++){
-        bst_tree(arr[i]);
+        bst(arr[i]);
     }
 }
 
-void printdata(node* root,string s){
-    if(root == NULL)
+void print_bst(node1 *start1,string s){
+    if(start1==NULL)
     return;
 
-    printdata(root->left,s+"->left");
-    cout<<s<<" "<<root->data<<endl;
-    printdata(root->right,s+"->right");
+    print_bst(start1->left,s+"->left");
+    cout<<s<<" "<<start1->data<<endl;
+    print_bst(start1->right,s+"->right");
 }
 
 int main(){
+    int n=0;
     cin>>n;
-    int data=0;
-    for(int i=0;i<n;i++){
-        cin>>data;
-    start = binary_tree(start,data);
+
+    create_ll(n);
+    int m=n;
+    struct node *middle=NULL;
+    if(m%2==1){
+        struct node *temp = start;
+        m = m/2;
+        while(m--){
+            temp = temp->next;
+        }
+        middle = temp;
+    }
+    else if(m%2==0){
+        struct node *temp = start;
+        m = m/2;
+        while(m--){
+            temp = temp->next;
+        }
+        middle = temp;
+    }
+    
+    int arr[n]={0};
+    arr[0] = middle->data;
+    struct node *temp = start;
+    int i=1;
+    while(temp!=middle){
+        arr[i] = temp->data;
+        i++;
+        temp = temp->next;
+    }
+    temp = middle->next;
+    while(temp!=NULL){
+        arr[i] = temp->data;
+        temp = temp->next;
+        i++;
     }
 
-    string s;
-
-    showdata(start);
-    printdata(start,s);
-
-    // cout<<"The array is :- "<<endl;
     // for(int i=0;i<n;i++){
-    //     cout<<arr[i]<<endl;
+    //     cout<<"i = "<<i<<" = "<<arr[i]<<endl;
     // }
-    cout<<endl;
 
-    //sort(arr,arr+n);
+    bst_tree_element(n,arr);
 
-    bst(arr);
-
-    printdata(root,s);
+    string s;
+    print_bst(start1,s);
 
 }
